86fc3f2020fd41ebb8308203bcfd44dda58432f2
Davi <davidtheblane@gmail.com>
2025-12-24 11:45:33 -0300

    tdd part 2

---
+++ aula-05-tdd-project-pt-02/seed/index.js
@@ -0,0 +1,51 @@
+const { faker } = require('@faker-js/faker');
+
+const Car = require('../src/entities/car');
+const Customer = require('../src/entities/customer');
+const CarCategory = require('../src/entities/carCategory');
+const { join } = require('path');
+const seederBaseFolder = join(__dirname, "../", "database");
+const {writeFile, mkdir} = require('fs/promises');
+
+const ITEMS_AMOUNT = 2;
+
+const carCategory = new CarCategory({
+  id: faker.string.uuid(),
+  name: faker.vehicle.type(),
+  carIds: [],
+  price: faker.finance.amount({ min: 20, max: 100 }),
+});
+
+const cars = [];
+const customers = [];
+
+for(let i = 0; i < ITEMS_AMOUNT; i++) {
+    const car = new Car({
+      id: faker.string.uuid(),
+      name: faker.vehicle.model(),
+      available: true,
+      gasAvailable: true,
+      realeaseYear: faker.date.past().getFullYear(),
+    });
+    carCategory.carIds.push(car.id);
+    cars.push(car);
+
+    const customer = new Customer({
+      id: faker.string.uuid(),
+      name: faker.person.fullName(),
+      age: faker.number.int({ min: 18, max: 50 }),
+    });
+    customers.push(customer);
+}
+
+const write = async (filename, data) => writeFile(join(seederBaseFolder, filename), JSON.stringify(data));
+
+(async () => {
+    await write('cars.json', cars);
+    await write('customers.json', customers);
+    await write('carCategories.json', [carCategory]);
+
+    console.log('carCategory', carCategory);
+    console.log('customers', customers);
+    console.log('cars', cars);
+})()
